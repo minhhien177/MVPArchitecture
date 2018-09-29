a8a0f2b07f238578fef484d4e03e3db91ccbc146
MinhHien Vo <minhhien177@gmail.com>
2018-09-29 21:51:07 +0800

    Initial Commit

---
+++ MVPArchitecture/MVPArchitecture.h
@@ -0,0 +1,19 @@
+//
+//  MVPArchitecture.h
+//  MVPArchitecture
+//
+//  Created by Hien Vo Minh on 29/9/18.
+//  Copyright © 2018 vomh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVPArchitecture.
+FOUNDATION_EXPORT double MVPArchitectureVersionNumber;
+
+//! Project version string for MVPArchitecture.
+FOUNDATION_EXPORT const unsigned char MVPArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVPArchitecture/PublicHeader.h>
+
+
